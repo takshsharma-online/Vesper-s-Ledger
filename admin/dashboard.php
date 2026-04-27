b04dbff80b9b346b8cc018ec9d5aebcb2b33f94a
Areesha Nur Sabryna Ahmad Salleh <sabrynasalleh@gmail.com>
2026-04-27 14:48:31 +0100

    Updated logout button

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,5 +1,13 @@
 <?php
     include 'admin_auth.php'; //Run authentication firstS
+    session_start();
+
+if (isset($_POST['logout'])) {
+    session_unset();
+    session_destroy();
+    header("Location: login.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -9,12 +17,14 @@
     </head>
     <body>
         <h1>MI6 Command Center</h1>
-        <p>Welcome, Commander.</p>
+        <p>Welcome.</p>
 
         <ul>
             <li><a href="manage_users.php">Manage Agents</a></li>
             <li><a href="audit_logs.php">View Logs</a></li>
-            <li><a href="../logout.php">Logout</a></li>
         </ul>
+        <form method="POST">
+            <button type="submit" name="logout">Logout</button>
+<       /form>
     </body>
 </html>
